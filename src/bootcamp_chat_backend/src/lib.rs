b3f7066e0c0a15df5aa907dfc6f6c5dfdc14205f
pu0238 <pu0238@protonmail.com>
2024-07-29 18:51:13 +0200

    Updated blockchain layer

--- src/bootcamp_chat_backend/src/lib.rs
+++ src/bootcamp_chat_backend/src/lib.rs
@@ -1,18 +1,32 @@
 use std::{cell::{Ref, RefCell}, collections::HashMap};
 
+use candid::Principal;
+use ic_cdk::caller;
 
 thread_local! {
-    static NOTES: RefCell<Vec<String>> = RefCell::default();
+    static NOTES: RefCell<HashMap<Principal, Vec<String>>> = RefCell::default();
 }
 
 #[ic_cdk::query]
-fn get_notes() -> Vec<String> {
-    NOTES.with_borrow(|notes| notes.clone())
+fn get_notes(user: Principal) -> Option<Vec<String>> {
+    NOTES.with_borrow(|notes|  notes.get(&user).cloned())
 }
 
 #[ic_cdk::update]
 fn add_note(note: String) {
+    let user = caller();
+
+    if user == Principal::anonymous() {
+        panic!("Anonymous Principal!")
+    }
+
     NOTES.with_borrow_mut(|notes| {
-        notes.push(note)
+        let mut_notes = notes.get_mut(&user);
+
+        if let Some(notes_vec) = mut_notes {
+            notes_vec.push(note);
+        } else {
+            notes.insert(user, vec![note]);
+        }
     })
 }

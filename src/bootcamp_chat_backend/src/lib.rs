04e1a1b87bf38c4f9ff4dfa4443e7933b662beb3
pu0238 <pu0238@protonmail.com>
2024-07-30 17:35:01 +0200

    In progress to add chats

--- src/bootcamp_chat_backend/src/lib.rs
+++ src/bootcamp_chat_backend/src/lib.rs
@@ -5,6 +5,35 @@ use ic_cdk::caller;
 
 thread_local! {
     static NOTES: RefCell<HashMap<Principal, Vec<String>>> = RefCell::default();
+    static CHAT: RefCell<HashMap<[Principal; 2], Vec<String>>> = RefCell::default();
+}
+
+#[ic_cdk::query]
+fn get_chat(user1: Principal, user2: Principal) -> Option<Vec<String>> {
+    CHAT.with_borrow(|chats|  chats.get(&[user1, user2]).cloned())
+}
+
+#[ic_cdk::update]
+fn add_chat_msg(msg: String, user2: Principal) {
+    let user1 = caller();
+
+    if user1 == Principal::anonymous() {
+        panic!("Anonymous Principal!")
+    }
+
+    let principals = [user1, user2];
+    principals.sort();
+
+
+    CHAT.with_borrow_mut(|chats| {
+        let mut_chat = chats.get_mut(&principals);
+
+        if let Some(chat_msgs) = mut_chat {
+            chat_msgs.push(msg);
+        } else {
+            chats.insert(principals, vec![msg]);
+        }
+    })
 }
 
 #[ic_cdk::query]
